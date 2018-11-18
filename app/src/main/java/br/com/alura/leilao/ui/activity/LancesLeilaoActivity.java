17810fa89ae109ba2b6c8279af01d7cc9b83519a
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-11-18 18:42:21 -0200

    Adicionando os lances

--- app/src/main/java/br/com/alura/leilao/ui/activity/LancesLeilaoActivity.java
+++ app/src/main/java/br/com/alura/leilao/ui/activity/LancesLeilaoActivity.java
@@ -1,11 +1,9 @@
 package br.com.alura.leilao.ui.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import java.io.Serializable;
 
 import br.com.alura.leilao.R;
 import br.com.alura.leilao.model.Leilao;
@@ -21,6 +19,13 @@ public class LancesLeilaoActivity extends AppCompatActivity {
             Leilao leilao = (Leilao) dadosRecebidos.getSerializableExtra("leilao");
             TextView descricao = findViewById(R.id.lances_leilao_descricao);
             descricao.setText(leilao.getDescricao());
+
+            TextView maiorLance = findViewById(R.id.lances_leilao_maior_lance);
+            maiorLance.setText(String.valueOf(leilao.getMaiorLance()));
+
+            TextView menosLance = findViewById(R.id.lances_leilao_menor_lance);
+            menosLance.setText(String.valueOf(leilao.getMenorLance()));
         }
     }
+
 }

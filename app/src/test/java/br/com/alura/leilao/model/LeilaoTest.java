acfc24dd60dd6d5434991c9626edd6811e212ee8
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2020-02-02 02:43:55 -0300

    Criando teste para leilão com vários lances em ordem crescente

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -22,4 +22,16 @@ public class LeilaoTest {
 
         assertEquals(200.0, maiorLanceDevolvido, 0.0001);
     }
+
+    @Test
+    public void getMaiorLanceComMaisDeUmLanceEmOrdemCrescenteDevolveMaiorLance() {
+        Leilao leilao = new Leilao("Console");
+        leilao.propoe(new Lance(new Usuario("Rodrigo"), 200.0));
+        leilao.propoe(new Lance(new Usuario("Luiza"), 300.0));
+        leilao.propoe(new Lance(new Usuario("Olavo"), 430.0));
+
+        double maiorLanceDevolvido = leilao.getMaiorLance();
+
+        assertEquals(430.0, maiorLanceDevolvido, 0.0001);
+    }
 }

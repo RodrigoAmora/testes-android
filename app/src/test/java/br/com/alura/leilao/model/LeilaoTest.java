accb73264eddfbea935b866d692cd21ba0dbf59f
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2020-02-02 01:59:16 -0300

    Criando teste de maior lance

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -12,4 +12,15 @@ public class LeilaoTest {
         String descricaoDevolvida = leilao.getDescricao();
         assertEquals("Console", descricaoDevolvida);
     }
+
+    @Test
+    public void getMaiorLance() {
+        Leilao leilao = new Leilao("Console");
+        leilao.propoe(new Lance(new Usuario("Rodrigo"), 200.0));
+        leilao.propoe(new Lance(new Usuario("Joao"), 300.0));
+
+        double maiorLanceDevolvido = leilao.getMaiorLance();
+
+        assertEquals(300.0, maiorLanceDevolvido, 0.0001);
+    }
 }

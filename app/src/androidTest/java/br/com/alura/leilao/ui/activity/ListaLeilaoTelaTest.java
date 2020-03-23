c681bfb4259500b95ae1be6b5acb96d6a32574f0
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2020-03-23 01:33:20 -0300

    Criando teste para leilão

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -1,5 +1,21 @@
-import static org.junit.Assert.*;
+package br.com.alura.leilao.ui.activity;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
 
 public class ListaLeilaoTelaTest {
 
+    @Rule
+    public ActivityTestRule activity = new ActivityTestRule(ListaLeilaoActivity.class, true, true);
+
+    @Test
+    public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaAPI() {
+        Espresso.onView(ViewMatchers.withText("Casa"))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
 }
